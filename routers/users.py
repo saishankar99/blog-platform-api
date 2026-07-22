9012d0411bd8fd2795880548af047241a781cf83
saishankar9999 <saikalyan.kurikuri@gmail.com>
2026-07-22 06:05:15 +0530

    Day 23: Added JWT token generation , /login method

--- routers/users.py
+++ routers/users.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordRequestForm
 from database import get_db
 import models
 import schemas
-from security import hash_password
+from security import hash_password,verify_password,create_access_token
 
 router = APIRouter(prefix="/users",tags=['users'])
 
@@ -21,3 +21,15 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.post("/login", response_model=schemas.Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email==form_data.username).first()
+
+    if user is None or not verify_password(form_data.password,user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Credentials")
+
+    access_token = create_access_token(data={"sub":user.email})
+
+    return {"access_token":access_token,"token_type":"bearer"}
+
--- security.py
+++ security.py
@@ -1,4 +1,13 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from config import settings
+from jose import jwt
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc)+timedelta(minutes=settings.access_token_expiry_minutes)
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode,settings.secret_key,algorithm=settings.algorithm)
 
 pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
 
